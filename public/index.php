b050356fcce9cbf6b59d9a8a3bdd7a722315b7c7
Brayan <bchacon959@gmail.com>
2024-11-04 20:38:38 -0600

    brayan

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php
-require_once 'controllers/UsuarioController.php';
+require_once '../src/controllers/UsuarioController.php';
 
 $usuarioController = new UsuarioController();
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET' && parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) === '/usuarios') {
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) === '/practica3/public/index.php/usuarios') {
     $usuarioController->getUsuarios();
 }
 ?>
